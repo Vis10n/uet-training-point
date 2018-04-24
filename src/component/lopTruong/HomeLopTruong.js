c884fd9b85d60dfc215875bbb0366f9b044f0e4d
piggy2303 <nguyentuananh23031997@gmail.com>
2018-04-24 13:49:13 +0700

    fix 24-4-2018

--- src/component/lopTruong/HomeLopTruong.js
+++ src/component/lopTruong/HomeLopTruong.js
@@ -74,7 +74,7 @@ class HomeLopTruong extends Component {
       <div>
         <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
           <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="">
-            UET training point
+            Monitor's Home
           </a>
           <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap">
